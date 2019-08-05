e5262cb77660d7ed2f89fa28f99ccdf5b60fe0f8
metafield <anthony.sylvester99@gmail.com>
2019-08-05 13:19:38 -0700

    configured dotenv and contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const dotenv = require("dotenv")
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config()
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
@@ -72,6 +78,13 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `9a0752b18app`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
